09f743d154db416a395a74cc9941e71c7b863f51
Alexey <apiburg@bk.ru>
2019-08-22 19:18:47 +0400

    Изменил конструктор инициализации AdMob

--- app/src/main/java/ru/slepova/goodmorning/Application.java
+++ app/src/main/java/ru/slepova/goodmorning/Application.java
@@ -7,6 +7,6 @@ public class Application extends android.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        MobileAds.initialize(this);
+        MobileAds.initialize(this,  "ca-app-pub-1874183522465795~9519015849");
     }
 }
